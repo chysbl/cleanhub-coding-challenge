3dafc0eda0205e1bf234bd3cb474398fdaae53b7
Charisse <charisse.ysabel930@gmail.com>
2022-07-27 19:38:13 +0200

    Build basic HubList

--- src/components/HubList/HubList.tsx
+++ src/components/HubList/HubList.tsx
@@ -1,3 +1,4 @@
+import { ReactComponent as Bottle } from "../../assets/icons/bottles.svg";
 import { Hub } from "../../types";
 import styles from "./HubList.module.scss";
 
@@ -8,12 +9,28 @@ export default function HubList({ hubs }: HubListProps) {
   return (
     <ul>
       {hubs.map((hub) => (
-        <HubListItem hub={hub} />
+        <HubListItem key={hub.uuid} hub={hub} />
       ))}
     </ul>
   );
 }
 
 function HubListItem({ hub }: { hub: Hub }) {
-  return <li className={styles.card}>{hub.uuid}</li>;
+  return (
+    <li className={styles.card}>
+      <span>{hub.parentHubName}</span>
+      <div className={styles.container}>
+        {hub.logo && (
+          <img src={hub.logo.directLink} alt="" className={styles.logo} />
+        )}
+        <div className={styles.content}>
+          <h1 className={styles.title}>{hub.displayName}</h1>
+          <span>
+            {hub.type} | {hub.location}
+          </span>
+          <Bottle />
+        </div>
+      </div>
+    </li>
+  );
 }
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,25 @@
+/**
+ * These interfaces are incomplete on purpose;
+ * only fields that have been used within the components
+ * are added. There is always the option to add the missing fields
+ * later on.
+ */
 export interface Hub {
     uuid: string;
+		cardDescription: string;
+		displayName: string;
+		formattedRecoveredQuantity?: string;
+		formattedTotalRecoveredQuantity?: string;
+		location: string;
+		logo: Logo;
+		name: string;
+		slug: string;
+		parentHubName?: string;
+		referenceQuantityUnit: string;
+		type: string;
+}
+
+export interface Logo {
+	directLink?: string;
+	uuid: string;
 }
